4d18cc6e05e25bfcfa7142ec770ea51bd95083ec
Akash Bawne <71551574+Welkinoid52@users.noreply.github.com>
2025-07-10 12:19:57 +0530

    Update C1.1.groovy

--- jobdsl/ProjectC/C1/C1.1.groovy
+++ jobdsl/ProjectC/C1/C1.1.groovy
@@ -2,7 +2,7 @@ folder('ProjectC')
 folder('ProjectC/C1')
 folder('ProjectC/C1/C1.1')
 
-job('ProjectC/C1/C1.1/c1.1_jenkins_job') {
+job('ProjectC/C1/C1.1/c1_1_jenkins_job') {
     description('Job for ProjectC → C1 → C1.1')
     steps {
         shell('echo Running c1.1_jenkins_job...')
